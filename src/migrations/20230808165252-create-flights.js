652fc5f6072b5b911e39e25a41d66c1ccc90d864
edwyncoder <edwyn.t@flyerssoft.com>
2023-08-08 22:26:23 +0530

    added flights model

---
+++ src/migrations/20230808165252-create-flights.js
@@ -0,0 +1,49 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Flights', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      flightNumber: {
+        type: Sequelize.STRING
+      },
+      airplaneId: {
+        type: Sequelize.INTEGER
+      },
+      departureAirportId: {
+        type: Sequelize.INTEGER
+      },
+      arrivalAirPortId: {
+        type: Sequelize.INTEGER
+      },
+      arrivalTime: {
+        type: Sequelize.DATE
+      },
+      departureTime: {
+        type: Sequelize.DATE
+      },
+      boardingGate: {
+        type: Sequelize.STRING
+      },
+      totalSeat: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Flights');
+  }
+};
