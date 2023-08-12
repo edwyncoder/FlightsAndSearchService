fc09a08112e6c61aa51476d37c756c7574bb0651
edwyncoder <edwyn.t@flyerssoft.com>
2023-08-12 13:04:03 +0530

    added error-codes

--- src/controllers/flight-controller.js
+++ src/controllers/flight-controller.js
@@ -1,5 +1,5 @@
 const {FlightService} = require('../services/index');
-// const { SuccessCodes } = require('../utils/error-codes');
+const { SuccessCodes } = require('../utils/error-codes');
 
 const flightService = new FlightService();
 
